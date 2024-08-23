1da624d58e45b6c5a8ec6cdd8b3802126e14d066
jylee <jylee@maxst.com>
2024-08-23 18:27:02 +0900

    누락된 파일 업로드

--- feature/detail/src/main/java/com/example/loldex/feature/detail/SavedCardToDeckScreen.kt
+++ feature/detail/src/main/java/com/example/loldex/feature/detail/SavedCardToDeckScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -103,7 +104,7 @@ fun SavedDeckTitleLayout(
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically
