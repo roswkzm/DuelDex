eecb72caf56f4a735363bae5f7ce8d0e72a50453
jylee <jylee@maxst.com>
2024-09-05 17:27:21 +0900

    SettingsDialog에 테마 변경 가능한 Radio 버튼 추가. 해당 Ridio 버튼 클릭으로 Theme 변경 가능

--- feature/home/src/main/java/com/example/loldex/feature/home/SettingsDialog.kt
+++ feature/home/src/main/java/com/example/loldex/feature/home/SettingsDialog.kt
@@ -1,15 +1,24 @@
 package com.example.loldex.feature.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -17,18 +26,21 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.loldex.core.designsystem.component.DefaultAlertDialog
 import com.example.loldex.core.designsystem.component.LdButton
 import com.example.loldex.core.designsystem.theme.Gray800
+import com.example.loldex.core.designsystem.theme.ThemePreviews
 import com.example.loldex.core.designsystem.theme.ldTypography
+import com.example.loldex.core.model.UserEnvData
+import com.example.loldex.core.model.enums.ThemeConfig
 
 @Composable
 fun SettingsDialog(
     onDismiss: () -> Unit,
     viewModel: SettingsViewModel = hiltViewModel()
 ) {
-
     val settingsUiState by viewModel.settingsUiState.collectAsStateWithLifecycle()
     SettingsContent(
         onDismiss = onDismiss,
         settingsUiState = settingsUiState,
+        onChangeThemeConfig = viewModel::setThemeConfig
     )
 }
 
@@ -36,6 +48,7 @@ fun SettingsDialog(
 fun SettingsContent(
     onDismiss: () -> Unit,
     settingsUiState: SettingsUiState,
+    onChangeThemeConfig: (ThemeConfig) -> Unit,
 ) {
     DefaultAlertDialog(
         onDismissRequest = onDismiss,
@@ -46,12 +59,35 @@ fun SettingsContent(
             )
         },
         text = {
-            HorizontalDivider()
-            Text(
-                modifier = Modifier.padding(vertical = 16.dp),
-                text = stringResource(id = R.string.settings_dialog_dark_mode_theme),
-                style = MaterialTheme.ldTypography.fontTitleS.copy(fontWeight = FontWeight.Bold),
-            )
+            when (settingsUiState) {
+                SettingsUiState.Loading -> {
+                    CircularProgressIndicator(modifier = Modifier)
+                }
+
+                is SettingsUiState.Success -> {
+                    var selectedThemeConfig = settingsUiState.appEnvData.themeConfig
+                    Column {
+                        HorizontalDivider()
+                        Text(
+                            modifier = Modifier.padding(vertical = 16.dp),
+                            text = stringResource(id = R.string.settings_dialog_dark_mode_theme),
+                            style = MaterialTheme.ldTypography.fontTitleS.copy(fontWeight = FontWeight.Bold),
+                        )
+
+                        Column(
+                            modifier = Modifier
+                        ) {
+                            for (theme in ThemeConfig.entries) {
+                                ThemeRadioButton(
+                                    text = theme.name,
+                                    selected = selectedThemeConfig == theme,
+                                    onClick = { onChangeThemeConfig(theme) }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         },
         confirmButton = {
             LdButton(
@@ -69,5 +105,45 @@ fun SettingsContent(
                 },
             )
         }
+    )
+}
+
+@Composable
+fun ThemeRadioButton(
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .wrapContentWidth()
+            .selectable(
+                selected = selected,
+                role = Role.RadioButton,
+                onClick = onClick
+            )
+            .padding(12.dp)
+    ) {
+        RadioButton(
+            selected = selected,
+            onClick = null
+        )
+        Spacer(Modifier.width(8.dp))
+        Text(text)
+    }
+}
+
+@ThemePreviews
+@Composable
+fun SettingsContentPreview() {
+    val settingsUiState = SettingsUiState.Success(
+        UserEnvData(
+            themeConfig = ThemeConfig.FOLLOW_SYSTEM
+        )
+    )
+    SettingsContent(
+        onDismiss = {},
+        settingsUiState = settingsUiState,
+        onChangeThemeConfig = {},
     )
 }
--- feature/home/src/main/java/com/example/loldex/feature/home/SettingsViewModel.kt
+++ feature/home/src/main/java/com/example/loldex/feature/home/SettingsViewModel.kt
@@ -4,11 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.loldex.core.data.repository.UserDataRepository
 import com.example.loldex.core.model.UserEnvData
+import com.example.loldex.core.model.enums.ThemeConfig
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,6 +25,12 @@ class SettingsViewModel @Inject constructor(
         initialValue = SettingsUiState.Loading,
         started = SharingStarted.WhileSubscribed(5_000),
     )
+
+    fun setThemeConfig(themeConfig: ThemeConfig) {
+        viewModelScope.launch {
+            userDataRepository.setThemeConfig(themeConfig)
+        }
+    }
 }
 
 sealed interface SettingsUiState {
