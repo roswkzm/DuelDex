f577dd8512e2248179263b11b7bdfb908b5b4f53
Jerry <jylee@maxst.com>
2024-09-11 14:07:36 +0900

    Release 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.dueldex"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 24091100
+        versionName = "1.0.0"
 
 //        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
