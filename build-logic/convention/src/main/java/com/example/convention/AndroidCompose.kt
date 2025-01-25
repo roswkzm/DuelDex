1196cea28147c193ccb53369f809cd39b6826249
Jerry <jylee@maxst.com>
2025-01-25 20:44:10 +0900

    Compose Multi-Module 대응
    외부모듈에 대해 UnStable 하다고 판단하는 Compose 특성 대응
    -> Model 데이터에 대해 stabilityConfigurationFiles를 사용해 컴파일 타임에 Stable하다고 간주

--- build-logic/convention/src/main/java/com/example/convention/AndroidCompose.kt
+++ build-logic/convention/src/main/java/com/example/convention/AndroidCompose.kt
@@ -46,5 +46,8 @@ internal fun Project.configureAndroidCompose(
         project.providers.gradleProperty("enableComposeCompilerReports").onlyIfTrue()
             .relativeToRootProject("compose-reports")
             .let(reportsDestination::set)
+
+        stabilityConfigurationFiles
+            .add(isolated.rootProject.projectDirectory.file("compose_compiler_config.conf"))
     }
 }
