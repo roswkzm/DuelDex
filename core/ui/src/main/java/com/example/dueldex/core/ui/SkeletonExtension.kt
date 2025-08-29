707b1de645593f479d2a6484f57d76e6587d5244
DT103958 <DT103958@mobis-partners.com>
2025-08-29 18:39:43 +0900

    SkeletonBox 잦은 Recomposition 발생에 따른 로직 수정
    drawWithCache를 통한 Recomposition 발생 제거

--- core/ui/src/main/java/com/example/dueldex/core/ui/SkeletonExtension.kt
+++ core/ui/src/main/java/com/example/dueldex/core/ui/SkeletonExtension.kt
@@ -1,41 +1,30 @@
 package com.example.dueldex.core.ui
 
-import androidx.compose.animation.core.LinearOutSlowInEasing
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.unit.dp
+import com.example.dueldex.core.designsystem.theme.ThemePreviews
 
 @Composable
 fun SkeletonBox(
-    modifier: Modifier = Modifier
-) {
-    SkeletonContent(
-        modifier = modifier
-    ) {
-        Box(
-            modifier = modifier
-        )
-    }
-}
-
-@Composable
-fun SkeletonContent(
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit = {}
 ) {
     Box(
         modifier = modifier.shimmerBackground()
@@ -44,27 +33,54 @@ fun SkeletonContent(
     }
 }
 
-fun Modifier.shimmerBackground(shape: Shape = RectangleShape): Modifier = composed {
-    val transition = rememberInfiniteTransition(label = "")
+@SuppressLint("SuspiciousModifierThen")
+fun Modifier.shimmerBackground(): Modifier = composed {
+    val transition = rememberInfiniteTransition(label = "shimmerBackground")
 
     val translateAnimation by transition.animateFloat(
         initialValue = 0f,
-        targetValue = 400f,
+        targetValue = 1000f,
         animationSpec = infiniteRepeatable(
-            tween(durationMillis = 1500, easing = LinearOutSlowInEasing),
-            RepeatMode.Restart
+            animation = tween(
+                durationMillis = 1200,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Restart
         ),
-        label = "",
+        label = "shimmerTranslate",
     )
-    val shimmerColors = listOf(
-        Color.LightGray.copy(alpha = 0.9f),
-        Color.LightGray.copy(alpha = 0.4f),
+
+    return@composed this.then(
+        drawWithCache {
+            val shimmerColors = listOf(
+                Color.LightGray.copy(alpha = 0.9f),
+                Color.LightGray.copy(alpha = 0.5f),
+                Color.LightGray.copy(alpha = 0.9f)
+            )
+
+            onDrawBehind {
+                val brush = Brush.linearGradient(
+                    colors = shimmerColors,
+                    start = Offset.Zero,
+                    end = Offset(x = translateAnimation, y = translateAnimation)
+                )
+
+                drawRect(
+                    brush = brush,
+                    size = this.size
+                )
+            }
+        }
     )
-    val brush = Brush.linearGradient(
-        colors = shimmerColors,
-        start = Offset(translateAnimation, translateAnimation),
-        end = Offset(translateAnimation + 100f, translateAnimation + 100f),
-        tileMode = TileMode.Mirror,
-    )
-    return@composed this.then(background(brush, shape))
+}
+
+@ThemePreviews
+@Composable
+private fun SkeletonBoxPreview() {
+    Column {
+        SkeletonBox(
+            modifier = Modifier
+                .size(200.dp)
+        )
+    }
 }
